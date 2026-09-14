99974d180aaf16ba409e61601b45c4ee389b316b
unknown <engrgauri@gmail.com>
2026-09-14 19:55:46 +0530

    Variable File

---
+++ variables.py
@@ -0,0 +1,30 @@
+# Create Python variables to store the following details
+
+name = "Gauri" # Your name
+
+age = 29 # Age
+
+city = "Amravati" # City
+
+Company_name = "Tech Mahindra" # College/company name
+
+current_learning_path = "Python" # Current learning topic
+
+experience = 4 # Years of experience or study
+
+like_python = True # Whether you like Python (True/False)
+
+expected_salary = 200000.25 # Your expected salary after learning Python
+
+# Then print the data type of every variable using type().
+
+print("Name - " + name ,type(name))
+print("Age - " , age ,type(age))
+print("City - " + city ,type(city))
+print("Company Name - " + Company_name ,type(Company_name))
+print("Current Learning Path - " + current_learning_path ,type(current_learning_path))
+print("Experience - " , experience ,type(experience))
+print("Do i like Python - " , like_python ,type(like_python))
+print("Expected Salary - " , expected_salary ,type(expected_salary))
+
+
