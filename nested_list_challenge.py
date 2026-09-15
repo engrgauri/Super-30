e9bc2919d03311aebd366ba93c783ec670491a8a
unknown <engrgauri@gmail.com>
2026-09-15 20:22:48 +0530

    Nested List Challenge

---
+++ nested_list_challenge.py
@@ -0,0 +1,18 @@
+students = [["Rahul", 21, "Python"],["Priya", 22, "Data Science"],["Aman", 20, "Machine Learning"]]
+
+# Print the complete list
+print(students)
+# Print Rahul's name
+print(students[0][0])
+# Print Priya's age
+print(students[1][1])
+# Print Aman's course
+print(students[2][2])
+# Print the complete record of Priya
+print(students[1])
+# Change Rahul's course to "AI"
+print(students[0][2])
+# Add another student record manually
+students.append(["Gauri",29,"Generative AI"])
+# Print the updated nested list
+print(students)
