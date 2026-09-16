19cf34d425fcfed62ac30f6e074e1bda0eb2d3d9
unknown <engrgauri@gmail.com>
2026-09-16 22:42:30 +0530

    Duplicate Data sets

---
+++ duplicate_data_sets.py
@@ -0,0 +1,17 @@
+numbers = [10, 20, 30, 20, 40, 10, 50, 30, 60]
+
+# Print the original list
+print(numbers)
+# Convert the list into a set
+nums = set(numbers)
+# Observe which duplicates disappear
+print(nums)
+# Convert the set back into a list
+print(list(nums))
+# Print the number of original elements
+print(len(numbers))
+# Print the number of unique elements
+print(len(nums))
+# Create another example using duplicate student names.
+studs = {"Gauri","Roshni","Roshni"}
+print(studs)
